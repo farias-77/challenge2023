3fa4eb64b0bdd80b2f747e3613b2b955aa2d799c
farias-77 <gabrielfalves2003@gmail.com>
2023-05-06 14:56:46 -0300

    feat: main server file configs

--- back/src/App.ts
+++ back/src/App.ts
@@ -3,11 +3,16 @@ import "express-async-errors";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import errorHandler from "./middlewares/errorHandlingMiddleware";
+import router from "./routers/index";
+
 const app = express();
 app.use(cors());
 app.use(json());
-
 dotenv.config();
+
+app.use(router);
+app.use(errorHandler);
 
 const PORT: number = Number(process.env.PORT);
 app.listen(PORT, () => {
