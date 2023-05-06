f56477da2f50bc790f7b117d60b0e2c137309292
farias-77 <gabrielfalves2003@gmail.com>
2023-05-06 14:03:09 -0300

    feat: custom messages for each error

--- back/src/schemas/authSchemas.ts
+++ back/src/schemas/authSchemas.ts
@@ -1,31 +1,41 @@
 import joi from "joi";
 
 export const signUpSchema = joi.object({
-    email: joi
-        .string()
-        .email()
-        .messages({
-            "string.empty": "Por favor, insira um e-mail",
-            "string.email": "Por favor, insira um e-mail válido",
-        })
-        .required(),
-    username: joi
-        .string()
-        .messages({ "string.empty": "Por favor, insira um username" })
-        .required(),
-    password: joi
-        .string()
-        .messages({ "string.empty": "Por favor, insira uma senha" })
-        .required(),
-    confirmPassword: joi
-        .string()
-        .messages({
-            "string.empty": "Por favor, insira uma confirmação de senha",
-        })
-        .required(),
+    name: joi.string().min(3).max(30).required().messages({
+        "string.base": "Nome deve ser um texto.",
+        "string.empty": "Nome não pode ser vazio.",
+        "string.min": "Nome deve ter no mínimo {#limit} caracteres.",
+        "string.max": "Nome deve ter no máximo {#limit} caracteres.",
+        "any.required": "Nome é obrigatório.",
+    }),
+    email: joi.string().email().required().messages({
+        "string.base": "E-mail deve ser um texto.",
+        "string.empty": "E-mail não pode ser vazio.",
+        "string.email": "E-mail inválido.",
+        "any.required": "E-mail é obrigatório.",
+    }),
+    password: joi.string().min(8).required().messages({
+        "string.base": "Senha deve ser um texto.",
+        "string.empty": "Senha não pode ser vazia.",
+        "string.min": "Senha deve ter no mínimo {#limit} caracteres.",
+        "any.required": "Senha é obrigatória.",
+    }),
+    confirmPassword: joi.any().equal(joi.ref("password")).required().messages({
+        "any.only": "Senhas não coincidem.",
+        "any.required": "Confirmar senha é obrigatório.",
+    }),
 });
 
-export const signInSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+const signInSchema = joi.object({
+    email: joi.string().email().required().messages({
+        "string.base": "E-mail deve ser um texto.",
+        "string.empty": "E-mail não pode ser vazio.",
+        "string.email": "E-mail inválido.",
+        "any.required": "E-mail é obrigatório.",
+    }),
+    password: joi.string().required().messages({
+        "string.base": "Senha deve ser um texto.",
+        "string.empty": "Senha não pode ser vazia.",
+        "any.required": "Senha é obrigatória.",
+    }),
 });
