bc4016c8bd846552ae11eceb27510344aa7e086d
farias-77 <gabrielfalves2003@gmail.com>
2023-05-06 14:20:41 -0300

    feat: token validation middleware

---
+++ back/src/middlewares/tokenValidationMiddleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export default function tokenMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const authorization: string = req.headers.authorization || "";
+        const token: string = authorization?.replace("Bearer ", "").trim();
+        const secretKey: string = process.env.JWT_SECRET || "";
+
+        if (!authorization) {
+            throw {
+                code: "unauthorized",
+                message: "You must send an Authorization header with a token!",
+            };
+        }
+
+        const retornoJWT = jwt.verify(token, secretKey);
+
+        res.locals.retornoJwtVerify = retornoJWT;
+        next();
+    } catch (error) {
+        throw { type: "unauthorized", message: "Invalid token." };
+    }
+}
