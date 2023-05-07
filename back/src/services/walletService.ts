84c2d98c0738a5ed6de0f4d277410774c1fd5f0e
farias-77 <gabrielfalves2003@gmail.com>
2023-05-06 23:06:44 -0300

    feat: service gets wallet by address

--- back/src/services/walletService.ts
+++ back/src/services/walletService.ts
@@ -12,3 +12,16 @@ export async function getWalletById(id: number) {
 
     return wallet;
 }
+
+export async function getWalletByAddress(publicAddress: string) {
+    const wallet = await walletRepository.getWalletByPublic(publicAddress);
+
+    if (!wallet) {
+        throw {
+            type: "not_found",
+            message: "Wallet not found",
+        };
+    }
+
+    return wallet;
+}
